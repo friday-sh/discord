1389a003819310ef57fae7c2895f4bf49d5389b7
Alex Overstreet <alex@overstreet.me>
2021-07-24 12:41:31 -0500

    Initial

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import { config } from "dotenv";
+config();
+import Discord from "discord.js";
+import fetch from "cross-fetch";
+
+interface Client extends Discord.Client {
+  owner: string;
+}
+
+const bot = new Discord.Client() as Client;
+bot.owner = "313731399968358401";
+
+const getCtx = async () => {
+  const data = await fetch(`http://localhost:6060/location`).then((res) =>
+    res.json()
+  );
+  return data;
+};
+
+let ctx: any = null;
+
+bot.on("ready", async () => {
+  console.log(`Logged in as ${bot.user.tag}`);
+  bot.user.setPresence({ status: "dnd" });
+  ctx = await getCtx();
+});
+
+bot.on("message", async (message) => {
+  if (message.content.toLowerCase().startsWith("f ")) {
+    const content = message.content
+      .toLowerCase()
+      .replace("f ", "")
+      .replace("+", "plus");
+
+    if (!content) return;
+
+    const query = encodeURI(content);
+
+    const data = await fetch("http://localhost:6060/v1", {
+      method: "POST",
+      body: JSON.stringify({
+        text: query,
+        discord: message.author.id,
+        location: ctx,
+      }),
+    }).then((res) => res.json());
+
+    if (data.result) message.channel.send(data.result);
+  }
+});
+
+bot.login(process.env.DISCORD_TOKEN);
